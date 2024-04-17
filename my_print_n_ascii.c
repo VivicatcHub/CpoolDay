7ba03b6f1de3128412233660e61f3f2eb6933439
Vivicaty <101065613+VivicatcHub@users.noreply.github.com>
2024-04-17 09:57:06 +0000

    test 2

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -3,7 +3,7 @@ void print_char(char c);
 void my_print_n_ascii(int howMany) {
     if (howMany < ('~' - '!')) {
         for (int i = '!'; i < ('!' + howMany); i++) {
-            print_char(i)
+            print_char(i);
         }
     }
 }
