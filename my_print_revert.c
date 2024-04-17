3073e74869498153fbcc0c9be108e0ff7d507058
Vivicaty <101065613+VivicatcHub@users.noreply.github.com>
2024-04-17 12:13:45 +0000

    exo3

--- my_print_revert.c
+++ my_print_revert.c
@@ -1,7 +1,17 @@
 void print_char(char c);
 
+int length(char *toRever) {
+    int i = 0;
+    while(toRever[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
 void my_print_revert(char *toRever) {
-    for (int i = (sizeof(toRever) / sizeof(toRever[0])) + 1; i >= 0; i--) {
-        print_char(toRever[i]);
+    if (toRever >= 0) {
+        for (int i = length(toRever) - 1; i >= 0; i--) {
+            print_char(toRever[i]);
+    }
     }
 }
