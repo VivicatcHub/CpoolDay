791d4969feec97028ff6539654914a7faa908462
Vivicaty <101065613+VivicatcHub@users.noreply.github.com>
2024-04-17 13:25:56 +0000

    Exo4

--- my_char_replace.c
+++ my_char_replace.c
@@ -1,5 +1,12 @@
 void print_char(char);
-int length(char *toRever);
+
+int length(char *toRever) {
+    int i = 0;
+    while(toRever[i] != '\0') {
+        i++;
+    }
+    return i;
+}
 
 char *my_char_replace(char *origin, char toFind, char toReplace) {
     for (int i = 0; i <= length(origin); i++) {
